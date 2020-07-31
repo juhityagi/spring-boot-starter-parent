dffb86fb498c05ddd815d02067a612f9a6440407
denyshubh <denyshubham@gmail.com>
2020-07-31 20:49:21 +0530

    removed bug from ShortSet Java

--- src/main/java/id/test/springboottesting/model/ShortSet.java
+++ src/main/java/id/test/springboottesting/model/ShortSet.java
@@ -11,7 +11,7 @@ public class ShortSet {
 	    Set<Short> s = new HashSet<>();
 	    for (short i = 0; i < 100; i++) {
 	      s.add(i);
-	      s.remove(i - 1);
+	    //   s.remove(i - 1);
 	    }
 	    System.out.println(s.size());
 	  }
